0e8e3f7ca5143ae4db042c42b5a955598bd9ebab
yuan2658 <2658147108@qq.com>
2020-01-03 09:21:12 +0800

    add

---
+++ src/test/java/com/utils/tools/ToolsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.utils.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("11111");
+    }
+
+}
